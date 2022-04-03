4e1d0d5fe18e9a8bd472363c8c3e4c13747fb1ca
Nayana Loivos <loivosdev@gmail.com>
2022-04-03 08:23:11 -0300

    organizando app.js e finalizando o backend]

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import notas from "./model/Nota.js";
 import routes from './routes/index.js';
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
@@ -9,27 +8,7 @@ db.once("open", () => {
 });
 
 const app = express();
-
 app.use(express.json());
-
 routes(app);
-
-// const notas = [
-//     {id: 1, "titulo": "Estudar", "conteudo": "API REST"},
-//     {id: 2, "titulo": "Cozinhar", "conteudo": "Panqueca"}
-// ]
-
-
-
-app.put('/notas/:id', (req, res) => {
-    let index = buscaNotas(req.params.id);
-    notas[index] = req.body.titulo;
-    notas[index] = req.body.conteudo;
-    res.status(200).json(notas);
-})
-
-function buscaNotas(id){
-    return notas.findIndex(nota => nota.id === id);
-}
 
 export default app;
